05f13f336657b281d0ae5ad2a20a7d06a5b4fcf7
hrh <1597669140@qq.com>
2023-07-07 10:31:17 +0800

    7—1迪普

--- 机试/7_1.c
+++ 机试/7_1.c
@@ -6,19 +6,6 @@ int cal(int i, int j, int k) {
     return i * 100 + j * 10 + k;
 }
 int main () {
-    char str1[10] = {'n', 'o', 'w'}, str2[] = "coder", str3[] = "abcde";
-    printf("%d%d%d\n", strlen(str1),strlen(str2),strlen(str3));
-    strcat(str1, str3);
-    printf("%s %d\n", str1, strlen(str1));
-    strcpy(str1 + strlen(str2), str2);
-    printf("%s %d\n", str1, strlen(str1));
-    /**
-    355
-    nowabcde 8
-    nowabcoder 10
-    */
-    // return 0;
-
     int cnt = 0;
     for (int i = 1; i <= 9; i += 1) {  // 百位数字（偶数，包括零）
         for (int j = 0; j <= 9; ++j) {  // 十位数字
